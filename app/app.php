103a5f6472697df32364bcffe16d27ed37a46020
nicolas <nickh93@outlook.com>
2016-09-27 14:10:34 -0400

    create Sylex routes and twig templates for user interface

--- app/app.php
+++ app/app.php
@@ -3,17 +3,32 @@
     require_once __DIR__."/../src/Cient.php";
     require_once __DIR__."/../src/Stylist.php";
 
+    $app = new Silex\Application();
+
     $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
-    return $app;
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
 
+    $app->post("/stylists", function () use ($app) {
+        $stylist = new Stylist($_POST['stylist-name']);
+        $stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get("stylists/{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+
+    return $app;
 ?>
